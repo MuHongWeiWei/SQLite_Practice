3865b011430e0307b756cf35af0676410e3a850a
MuHongWeiWei <4a490094@stust.edu.tw>
2019-02-23 00:19:14 +0800

    Initial commit

---
+++ app/src/main/java/com/example/datebasepractice/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.datebasepractice;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MyOpenHelper helper = new MyOpenHelper(getApplicationContext());
+
+        SQLiteDatabase writeDatabase = helper.getWritableDatabase();
+    }
+}
---
+++ app/src/main/java/com/example/datebasepractice/MyOpenHelper.java
@@ -0,0 +1,21 @@
+package com.example.datebasepractice;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class MyOpenHelper extends SQLiteOpenHelper {
+    public MyOpenHelper(Context context) {
+        super(context, "test.db", null, 2);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table info(_id integer primary key autoincrement, name varchar(20))");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("alter table info add phone varchar(20)");
+    }
+}
